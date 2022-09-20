7b5c0317faf01841cf133794a263ead08cf06950
sindrimt <sindrimt@gmail.com>
2022-09-20 16:02:23 +0200

    Basic setup of react routing
    Co-authored-by: Ragnar Dreier <ragnad@stud.ntnu.no>
    Co-authored-by: marcusn1301 <marcussn@stud.ntnu.no
    >
    Co-authored-by: kelvinghm <kgma@stud.ntnu.no
    >

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
 
+import Commits from "./components/Commits";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const App = () => {
-    return <div>React test app</div>;
+    return (
+        <BrowserRouter>
+            <div>Navbar</div>
+            <Routes>
+                <Route path="/" element={<div>Homepage</div>} />
+                <Route path="/commits" element={<Commits />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
---
+++ src/components/Commits.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Commits = () => {
+    return <div>Commits</div>;
+};
+
+export default Commits;
