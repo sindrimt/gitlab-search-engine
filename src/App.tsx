449f110bfecc136c3ec26562d037b24ddd71ec1e
Marcus Stephan Nordal <marcusn1301@gmail.com>
2022-09-26 02:54:20 +0200

    Setup homepage and added to routing Related to #2

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Commits from "./components/Commits";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Homepage from "./pages/Homepage/Homepage";
+
 const App = () => {
     return (
         <BrowserRouter>
             <div>Navbar</div>
             <Routes>
-                <Route path="/" element={<div>Homepage</div>} />
+                <Route path="/" element={<Homepage />} />
                 <Route path="/commits" element={<Commits />} />
             </Routes>
         </BrowserRouter>
