ab0ba2b105f86d36cf936d1d00c5bec4e7051bc8
sindrimt <sindrimt@gmail.com>
2022-09-27 17:23:41 +0200

    Implemented the api data into the graph
    Related to #8
    
    Co-authored-by: Kelvin Gia Huy Ma <kgma@stud.ntnu.no>

--- src/components/Charts/AnotherChart.tsx
+++ src/components/Charts/AnotherChart.tsx
@@ -1,53 +1,46 @@
+import React, { useContext } from "react";
 import ReactFC from "react-fusioncharts";
 import FusionCharts from "fusioncharts";
 import Chart from "fusioncharts/fusioncharts.charts";
 import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+import { ContextState } from "../../context/ContextState";
 
 ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
-const dataSource = {
-    chart: {
+const AnotherChart = (): any => {
+    const [repositoryInformation, setRepositoryInformation] = useContext(ContextState);
+
+    let dataSource: any = {};
+
+    dataSource["chart"] = {
         caption: "Overview of commits",
         subcaption: '<a href="https://gitlab.stud.idi.ntnu.no/it2810-h22/Team-40/prosjekt-2/">Link to repository</a>',
-        plottooltext: "<b>$dataValue</b> of users use $label",
-        showlegend: "1",
-        numbersuffix: "%",
+        numbersuffix: " commits",
         legendposition: "bottom",
         usedataplotcolorforlabels: "1",
         theme: "fusion",
         enablemultislicing: "0",
-    },
-    data: [
-        {
-            label: "Chrome",
-            value: "58.90",
-        },
-        {
-            label: "Firefox",
-            value: "13.29",
-        },
-        {
-            label: "Internet Explorer",
-            value: "13",
-        },
-        {
-            label: "Edge",
-            value: "3.78",
-        },
-        {
-            label: "Safari",
-            value: "3.42",
-        },
-        {
-            label: "Others",
-            value: "5.96",
-            tooltext:
-                "<b>$dataValue</b> of users use below browsers:<br>• Sogou Explorer: <b>1.68%</b><br>• Opera: <b>1.57%</b><br>• QQ: <b>1.35%</b><br>• UC Browser: <b>0.73%</b><br>• Yandex: <b>0.63%</b>",
-        },
-    ],
-};
+    };
 
-const AnotherChart = () => {
+    dataSource["data"] = [];
+
+    const filteredMembersWithCommits = repositoryInformation?.members?.filter((member: any) => {
+        return member?.commitCount > 0;
+    });
+
+    console.log(filteredMembersWithCommits);
+
+    filteredMembersWithCommits?.map((member: any) => {
+        dataSource.data.push({
+            label: member?.name,
+            value: member?.commitCount,
+        });
+    });
+
+    console.log("COMMIT DSTA");
+    console.log(dataSource);
+    //resolve(commitData);
+
     const chartConfigs = {
         type: "pie3d",
         width: "100%",
