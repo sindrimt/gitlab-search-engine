b7e40b0457c6c6c1818e468428842be45d5ff6fb
Marcus Stephan Nordal <marcusnordal@icloud.com>
2022-10-04 16:43:18 +0200

    #12 Navbar test changes + tsx

--- src/__test__/Navbar.test.tsx
+++ src/__test__/Navbar.test.tsx
@@ -1,13 +1,10 @@
-import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 
-import "@testing-library/jest-dom";
 import Navbar from "../components/Navbar/Navbar";
 
 describe("Navbar", () => {
-    it("Navbar rednered successfully", () => {
-        const { NavbarOuter } = render(<Navbar />);
-        //expect(NavbarOuter).toEqual("Welcome to GITLAB SEARCH ENGINE");
+    it("Check if corect text is rendered in navbar", () => {
+        render(<Navbar />);
         const navbarWelcome = screen.getByText("Welcome to");
         const navbarGitlab = screen.getByText("GITLAB SEARCH ENGINE");
         expect(navbarWelcome).toBeInTheDocument();
